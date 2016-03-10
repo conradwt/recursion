72aadef345946fb323117608ffe8f61e60918719
Conrad Taylor <conradwt@gmail.com>
2016-03-10 14:43:53 -0800

    Initial commit of recursion repository using Elixir.

---
+++ README.md
@@ -0,0 +1,20 @@
+# Recursion
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
+
+  1. Add recursion to your list of dependencies in `mix.exs`:
+
+        def deps do
+          [{:recursion, "~> 0.0.1"}]
+        end
+
+  2. Ensure recursion is started before your application:
+
+        def application do
+          [applications: [:recursion]]
+        end
+
---
+++ test/recursion_test.exs
@@ -0,0 +1,61 @@
+defmodule RecursionTest do
+  use ExUnit.Case
+
+  test "length on empty list" do
+    subject = []
+    
+    result = Recursion.list_length( subject )
+    
+    assert 0 == result
+  end
+  
+  test "length on non-empty list" do
+    subject = [1,2,3,4,5,6,7,8,9]
+
+    result = Recursion.list_length( subject )
+
+    assert 9 == result
+  end
+  
+  test "sum of an empty list" do
+    subject = []
+    
+    result = Recursion.sum( subject )
+    
+    assert 0 == result
+  end
+  
+  test "sum of a non-empty list" do
+    subject = [1,2,3,4,5,6,7,8,9]
+    
+    result = Recursion.sum( subject )
+    
+    assert 45 == result
+  end
+  
+
+  test "factorial of 0" do
+    subject = 0
+    
+    result = Recursion.factorial( subject )
+    
+    assert 1 == result
+  end
+  
+  test "factorial of 1" do
+    subject = 1
+    
+    result = Recursion.factorial( subject )
+    
+    assert 1 == result
+  end
+  
+  test "factorial of a number greater than 0" do
+    subject = 10
+    
+    result = Recursion.factorial( subject )
+    
+    assert 3_628_800 == result
+  end
+    
+end
