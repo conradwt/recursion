e3b9ef4bdd33cbb5e427f0c31cf271ded5203f5e
Conrad Taylor <conradwt@gmail.com>
2016-09-16 15:34:02 -0700

    Add recursive power function and associated unit tests.

--- lib/recursion.ex
+++ lib/recursion.ex
@@ -1,38 +1,48 @@
 defmodule Recursion do
-  
+
   #
   # list_length
   #
-  
+
   def list_length( collection ), do: _list_length( collection, 0 )
-  
+
   defp _list_length( [], total ), do: total
   defp _list_length( [ head | tail ], total ) do
     _list_length( tail, total + 1 )
   end
-  
+
   #
   # sum
   #
-  
+
   def sum( collection ), do: _sum( collection, 0 )
-  
+
   defp _sum( [], accumulator ), do: accumulator
   defp _sum( [ head | tail ], accumulator ) do
     _sum( tail, head + accumulator )
   end
-  
+
   #
   # factorial
   #
-  
+
   def factorial( number ), do: _factorial( number, 1 )
-  
-  def _factorial( 0, value ), do: value
-  def _factorial( 1, value ), do: value
-  def _factorial( number, value ) do
+
+  defp _factorial( 0, value ), do: value
+  defp _factorial( 1, value ), do: value
+  defp _factorial( number, value ) do
     _factorial( number - 1, number * value )
   end
-      
-end
 
+  #
+  # power
+  #
+
+  def power( number, power ), do: _power(number, power, 1)
+
+  defp _power( number, 0, value ), do: value
+  defp _power( number, power, value) do
+    _power( number, power - 1, number * value )
+  end
+
+end
--- test/recursion_test.exs
+++ test/recursion_test.exs
@@ -3,12 +3,12 @@ defmodule RecursionTest do
 
   test "length on empty list" do
     subject = []
-    
+
     result = Recursion.list_length( subject )
-    
+
     assert 0 == result
   end
-  
+
   test "length on non-empty list" do
     subject = [1,2,3,4,5,6,7,8,9]
 
@@ -16,44 +16,62 @@ defmodule RecursionTest do
 
     assert 9 == result
   end
-  
+
   test "sum of an empty list" do
     subject = []
-    
+
     result = Recursion.sum( subject )
-    
+
     assert 0 == result
   end
-  
+
   test "sum of a non-empty list" do
     subject = [1,2,3,4,5,6,7,8,9]
-    
+
     result = Recursion.sum( subject )
-    
+
     assert 45 == result
   end
 
   test "factorial of 0" do
     subject = 0
-    
+
     result = Recursion.factorial( subject )
-    
+
     assert 1 == result
   end
-  
+
   test "factorial of 1" do
     subject = 1
-    
+
     result = Recursion.factorial( subject )
-    
+
     assert 1 == result
   end
-  
+
   test "factorial of a number greater than 0" do
     subject = 10
-    
+
     result = Recursion.factorial( subject )
-    
+
     assert 3_628_800 == result
+  end
+
+  test "a number being rasied by the 0" do
+    number = 10
+    power = 0
+
+    result = Recursion.power(number, power)
+
+    assert 1 == result
+  end
+
+  test "a number being raised being raised to power > 0" do
+    number = 10
+    power  = 3
+
+    result = Recursion.power( number, power )
+
+    assert 1000 == result
   end
 end
